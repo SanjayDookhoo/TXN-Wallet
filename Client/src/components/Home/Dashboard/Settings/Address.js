ba1f90b335299ff6304f57c90db0bffb7d0bb4b3
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2021-06-24 02:49:41 -0400

    clean up of code byu seperation of components

---
+++ Client/src/components/Home/Dashboard/Settings/Address.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faTrash,
+	faEdit,
+	faPlus,
+	faMinus,
+} from '@fortawesome/free-solid-svg-icons';
+
+const initialState = {
+	name: '',
+	address_hash: '',
+};
+
+const Address = ({
+	address,
+	handleAddressUpdate,
+	handleAddressRemove,
+	editing_id,
+}) => {
+	return (
+		<tr
+			className={`address ${editing_id === address.id && 'bg-green-700'}`}
+		>
+			<td className="px-1 border border-green-800">{address.name}</td>
+			<td className="px-1 break-all border border-green-800">
+				{address.address_hash}
+			</td>
+			<td className="px-1 border border-green-800">
+				<div className="flex justify-between items-center">
+					<div
+						className="address-update cursor-pointer waves-effect rounded-lg p-2 hover:text-yellow-400"
+						onClick={() => handleAddressUpdate(address.id)}
+					>
+						<FontAwesomeIcon icon={faEdit} />
+					</div>
+					<div
+						className="address-delete cursor-pointer waves-effect rounded-lg p-2 hover:text-red-400"
+						onClick={() => handleAddressRemove(address.id)}
+					>
+						<FontAwesomeIcon icon={faTrash} />
+					</div>
+				</div>
+			</td>
+		</tr>
+	);
+};
+
+export default Address;
