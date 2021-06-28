97c9eee917434c7e8a29bb2c387fc33c13901993
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2021-06-28 03:35:53 -0400

    previously mobile and desktop existed as two seperate instances with different states, prevented that from happening

--- Client/src/components/Home/MainContainer.js
+++ Client/src/components/Home/MainContainer.js
@@ -8,37 +8,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { signOut } from '../../ducks/actions/auth';
 
+const div_container =
+	'h-main-cont bg-gray-200 rounded-lg overflow-hidden shadow-2xl';
+const div_container_desktop = 'md:w-main-cont';
+const div_container_mobile = 'w-96 ';
+
+const inner_div_container = 'w-full h-full flex justify-start';
+const inner_div_container_desktop = 'md:flex-row';
+const inner_div_container_mobile = 'flex-col ';
+
+const body_container = 'content overflow-y-auto border-gray-600 flex-grow';
+const body_container_desktop =
+	'md:p-16 md:border-t-0 md:border-b-0 md:border-l-4';
+const body_container_mobile = 'p-2 border-t-4 border-b-4';
+
 const MainContainer = ({ body, navbar, ...props }) => {
 	return (
 		<div className="window-container flex flex-col justify-center items-center h-screen w-screen bg-yellow-500">
-			{/* desktop container */}
-			<div className="w-main-cont h-main-cont hidden md:block bg-gray-200 rounded-lg overflow-hidden shadow-2xl">
-				<div className="w-full h-full flex flex-row justify-start">
-					<div className="navbar w-24 flex flex-col justify-between items-center">
+			<div
+				className={`${div_container} ${div_container_mobile} ${div_container_desktop}`}
+			>
+				<div
+					className={`${inner_div_container} ${inner_div_container_mobile} ${inner_div_container_desktop}`}
+				>
+					<div className="navbar w-24 hidden md:flex flex-col justify-between items-center">
 						<LogoButton />
 						<div className="flex flex-col justify-between items-center">
 							{navbar}
 						</div>
 						<LogoutButton />
 					</div>
-					<div className="content overflow-y-auto p-16 border-l-4 border-gray-600 flex-grow">
-						{body}
-					</div>
-				</div>
-			</div>
-			{/* mobile container */}
-			<div className="w-96 h-main-cont md:hidden bg-gray-200 rounded-lg overflow-hidden shadow-2xl">
-				<div className="w-full h-full flex flex-col justify-start">
-					<div className="navbar h-12 flex flex-row justify-center items-center rounded-tr-lg rounded-tl-lg border-t-2 border-l-2 border-r-2 border-gray-600 bg-yellow-500">
+					<div className="navbar md:hidden h-12 flex flex-row justify-center items-center rounded-tr-lg rounded-tl-lg border-t-2 border-l-2 border-r-2 border-gray-600 bg-yellow-500">
 						<LogoButton />
 					</div>
 					<div
 						id="mobile-content-swipe-anchor"
-						className="content overflow-y-auto p-2 border-t-4 border-b-4 border-gray-600 flex-grow"
+						className={`${body_container} ${body_container_desktop} ${body_container_mobile}`}
 					>
 						{body}
 					</div>
-					<div className="flex flex-row justify-between items-center">
+					<div className="flex md:hidden flex-row justify-between items-center">
 						{navbar}
 						<LogoutButton />
 					</div>
