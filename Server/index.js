082a955969a363765a51e95432f1dbd6b9897d99
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2021-06-28 21:21:41 -0400

    testing deploy

--- Server/index.js
+++ Server/index.js
@@ -49,7 +49,7 @@ app.use(cors());
 app.use('/user', user_router);
 app.use('*', auth, general_router);
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
 	console.log(`Server Running on Port: http://localhost:${PORT}`)
 );
