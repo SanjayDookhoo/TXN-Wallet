aa950b8792e0c66e341fc884671fbd8d940245cf
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2022-11-15 12:24:07 -0400

    https://api.txnwallet.me endpoint

--- Client/src/ducks/api/database.js
+++ Client/src/ducks/api/database.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'http://localhost:5000' });
 // const API = axios.create({ baseURL: 'http://192.168.0.10:5000' });
-const API = axios.create({ baseURL: 'https://txn-wallet.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://api.txnwallet.me' });
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem('profile')) {
